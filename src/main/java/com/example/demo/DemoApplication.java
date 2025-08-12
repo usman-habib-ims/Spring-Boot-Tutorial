4379c523aa289bf91bd19334e5ac51558a955c84
Usman Habib <usmanhabib802@gmail.com>
2025-08-13 01:58:10 +0500

    Api Layer , Student Controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args)
@@ -19,17 +12,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<Student> hello(){
-		return List.of(
-				new Student(
-						1L,
-						"Anonymous",
-						"Anonymous@gmail.com",
-						LocalDate.of(2000 , Month.FEBRUARY , 29),
-						14
-				)
-		);
-	}
 }
-// Comment testing
